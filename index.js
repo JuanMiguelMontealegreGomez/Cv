aea9e38688e2c0722aa883db6817787ffb5245db
Juan Miguel <3152719060502@ingenieria.usac.edu.gt>
2021-05-07 18:56:36 -0600

    adding files

---
+++ index.js
@@ -0,0 +1,17 @@
+function verPerfil()
+{
+    window.open("https://github.com/JuanMiguelMontealegreGomez");
+    console.log("Accediendo a Github.");
+}
+
+function enviarCorreo() 
+{
+    window.open('mailto:202000402.juan.montealegre@gmail.com');
+    console.log("Accediendo al cliente de correo");
+}
+
+function verIG()
+{
+    window.open("https://www.instagram.com/migu3_mg/");
+    console.log("Accediendo a Instagram")
+}
